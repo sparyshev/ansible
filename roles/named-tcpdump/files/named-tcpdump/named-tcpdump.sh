4351c0ad20df3045ede8209e7ae8e508910c21c3
taras.sparyshev <taras.sparyshev@gmail.com>
2021-04-14 18:32:43 +0300

    first role with molecule

---
+++ roles/named-tcpdump/files/named-tcpdump/named-tcpdump.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source settings
+
+function funcLogDir {
+        mkdir -p $LOG_DIR && echo "$LOG_DIR has been created"
+        touch $LOG_DIR$LOG_FILE && echo "$LOG_DIR$LOG_FILE has been created"
+        chmod 777 $LOG_DIR$LOG_FILE && echo `ls -la $LOG_DIR$LOG_FILE`
+}
+
+function funcTcpdump {
+        cmd=`$TCPDUMP_BIN $TCPDUMP_OPTIONS | grep -B 1 --line-buffered -E ": 2 [0-9]" >> $LOG_DIR$LOG_FILE &`
+        echo $cmd
+}
+
+if [ -e $LOG_DIR$LOG_FILE ]
+        then
+                funcTcpdump
+
+        else
+                funcLogDir
+                funcTcpdump
+fi
